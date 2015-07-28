c5ddd43a138fa7f2dacf136a8b96d1fc14018c8a
Ubuntu <ubuntu@ip-172-31-20-252.us-west-2.compute.internal>
2015-07-28 19:34:24 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.use(express.cookieParser());
+app.get('/sad', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  res.cookie('cookieName','asdasdasd', { maxAge: 900000, httpOnly: true });
+  console.log('Example app listening at http://%s:%s', host, port);
+});
